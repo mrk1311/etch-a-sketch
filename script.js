dc0d6f389a041e73778462cf202b6e93ccebdc38
mrk <mpruszkowski11@gmail.com>
2023-10-12 15:23:24 +0200

    better ui, added eraser, rainbow mode, hide grid

--- script.js
+++ script.js
@@ -1,31 +1,84 @@
 const gridContainer = document.querySelector('#grid-container');
 const body = document.querySelector('body');
+let gridSize = 16;
+let gridVisible = true;
+
+// TODO add a hide/show grid button
+const gridButton = document.querySelector('#grid-btn');
+gridButton.addEventListener('click', () => {
+    if (gridVisible === true) {
+        hideGrid();
+        gridButton.textContent = 'Show Grid';
+    } else {
+        showGrid();
+        gridButton.textContent = 'Hide Grid';
+    }
+});
+
+// add a eraser button
+const eraserButton = document.querySelector('#eraser-btn');
+let eraser = false;
+eraserButton.addEventListener('click', () => {
+    if (eraser === true) {
+        return;
+    } else {
+        eraser = true;
+        rainbowMode = false;
+    }
+});
+
+// color mode
+const colorButton = document.querySelector('#color-btn');
+colorButton.addEventListener('click', () => {
+    eraser = false;
+    rainbowMode = false;
+});
+
+// rainbow mode
+const rainbowButton = document.querySelector('#rainbow-btn');
+let rainbowMode = false;
+rainbowButton.addEventListener('click', () => {
+    if (rainbowMode === true) {
+        return;
+    } else {
+        rainbowMode = true;
+        eraser = false;
+    }
+
+});
+
+const modeButtons = document.querySelectorAll('.mode');
+modeButtons.forEach((modeButton) => {
+    modeButton.addEventListener('click', () => {
+        modeButtons.forEach((modeButton) => {
+            modeButton.classList.remove('selected');
+        });
+        modeButton.classList.add('selected');
+    });
+});
+
 
 // add a reset button
 
 const resetButton = document.querySelector('#reset-btn');
 resetButton.addEventListener('click', () => {
-    createGrid(newGridSize);
+    createGrid(gridSize);
 });
 
 // change grid size when user changes the range slider
-
-const sizePicker = document.querySelector('#size-picker');
-sizePicker.addEventListener('change', () => {
-    newGridSize = sizePicker.value;
-    createGrid(newGridSize);
-});
-
 // change the grid size label when slider moves
 
+const sizePicker = document.querySelector('#size-picker');
 const sizePickerContainer = document.querySelector('#size-picker-container');
 const sizePickerLabel = document.querySelector('#size-picker-label');
 sizePicker.addEventListener('input', () => {
+    gridSize = sizePicker.value;
+    createGrid(gridSize);
     sizePickerLabel.textContent = sizePicker.value + ' x ' + sizePicker.value;
 });
 
 
-createGrid(16);
+createGrid(gridSize);
 
 function createGrid(size) {
 
@@ -79,7 +132,39 @@ function createGrid(size) {
 // paint a cell
 
 function paint(e) {
-    color = document.getElementById('color-picker').value;
+
+    if (rainbowMode === false && eraser === false) {
+        color = document.getElementById('color-picker').value;
+    } else if (eraser === true){
+        color = '#BFC0C0';
+    } else if (rainbowMode === true) { 
+        color = getRandomColor();
+    }
+
     e.target.style.backgroundColor = color;
 }
 
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i=0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function hideGrid() {
+    gridVisible = false;
+    const gridCells = document.querySelectorAll('.grid-cell');
+    gridCells.forEach((gridCell) => {
+        gridCell.classList.add('borderless');
+    });
+}
+
+function showGrid() {
+    gridVisible = true;
+    const gridCells = document.querySelectorAll('.grid-cell');
+    gridCells.forEach((gridCell) => {
+        gridCell.classList.remove('borderless');
+    });
+}
